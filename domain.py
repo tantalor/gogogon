f10cb7a65b4862bbcff094bcebe8a332547cfcfb
John Tantalo <john.tantalo@gmail.com>
2011-07-07 06:58:25 -0700

    url -> domain

---
+++ domain.py
@@ -0,0 +1,20 @@
+from urlparse import urlparse
+import re
+import unittest
+
+DOMAIN_RE = re.compile('([^.]+\.[^.]+)$')
+
+def domain(url):
+  """Returns the two-level domain part of a URL."""
+  netloc = urlparse(url)[1]
+  match = DOMAIN_RE.search(netloc)
+  if match:
+    return match.groups()[0]
+
+class TestDomain(unittest.TestCase):
+  def testDomain(self):
+    self.assertEquals(domain("http://spam.eggs"), "spam.eggs")
+    self.assertEquals(domain("http://foo.bar.baz"), "bar.baz")
+
+if __name__ == '__main__':
+  unittest.main()

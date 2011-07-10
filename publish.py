eb768c5f3f3fabd0770ebf141a9fb90f6a24491d
John Tantalo <john.tantalo@gmail.com>
2011-07-09 21:07:21 -0700

    lookup url, json output

--- publish.py
+++ publish.py
@@ -4,23 +4,45 @@ import datetime
 import os
 import sys
 import subprocess
+import bitly
+from domain import domain
+import json
+
+GROUPSIZE = 10
 
 def main():
   today = datetime.datetime.today()
   one_day = datetime.timedelta(1)
   yesterday = today - one_day
   
+  # find yesterday's log
   logfile = "/var/log/gogogon/consumer.log.%04d-%02d-%02d" % \
     (yesterday.year, yesterday.month, yesterday.day)
-  
   if not os.path.exists(logfile): return
   
-  cmd = 'grep INFO %s | cut -f 4 -d " " | sort | uniq -c' % logfile
+  # sort and uniq the log
+  cmd = 'tail -10 %s | grep INFO | cut -f 4-5 -d " " | sort | uniq -c' % logfile
   pipe = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
   
+  # collect up global hashes and click counts
+  details = dict()
   for line in pipe.stdout:
-    (count, global_hash) = line.strip().split()
-    print (count, global_hash)
+    (count, global_hash, url) = line.strip().split()
+    details[global_hash] = dict(
+      u=url,
+      global_clicks=count,
+      agency=domain(url),
+      global_hash=global_hash,
+    )
+  
+  # grab hashes in groups of GROUPSIZE size
+  for i in xrange(1+len(details)/GROUPSIZE):
+    hashes = details.keys()[i*GROUPSIZE:i*GROUPSIZE+GROUPSIZE]
+    # lookup titles
+    for info in bitly.info(*hashes):
+      details[info['hash']]['title']=info['title']
+  
+  print "\n".join(json.dumps(d) for d in details.values())
 
 if __name__ == '__main__':
   main()

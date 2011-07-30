421e8d9db981f94f58d9fbad01bbbda43a4fc075
Richard Bullington-McGuire <richard@bullington-mcguire.com>
2011-07-30 14:58:35 -0400

    Refactor code to be cleaner, backwards compatible

--- ranks.py
+++ ranks.py
@@ -8,34 +8,37 @@ import bitly
 from domain import domain
 import json
 import csv
+import optparse
 
 GROUPSIZE = 10
 file_name_prefix = "/var/log/gogogon/consumer.log"
-output_directory = "/var/log/gogogon/ranking"
-
-# Credit for this goes to nosklo: 
-#  http://stackoverflow.com/questions/635483/what-is-the-best-way-to-implement-nested-dictionaries-in-python/652284#652284
-# 
-
-class AutoVivification(dict):
-    """Implementation of perl's autovivification feature."""
-    def __getitem__(self, item):
-        try:
-            return dict.__getitem__(self, item)
-        except KeyError:
-            value = self[item] = type(self)()
-            return value
-
+default_output_directory = "/var/log/gogogon/ranking"
 
 def main():
   today = datetime.datetime.today()
   one_day = datetime.timedelta(1)
   yesterday = today - one_day
-  
-  # find yesterday's log
-  logfile = file_name_prefix + ".%04d-%02d-%02d" % \
+  iso_date = "%04d-%02d-%02d" % \
     (yesterday.year, yesterday.month, yesterday.day)
-  if not os.path.exists(logfile): return
+  # Use yesterday's log by default
+  logfile = file_name_prefix + "." + iso_date
+
+  # But allow this to be overridden
+  parser = optparse.OptionParser()
+  parser.add_option('-f', '--file', dest="logfile", 
+                    default=logfile)
+  parser.add_option('-o', '--output-directory', dest="output_directory", 
+                    default= default_output_directory)
+  parser.add_option('-a', '--agency', dest="use_agency_domain", 
+                    default=False)
+  options, remainder = parser.parse_args()
+  logfile = options.logfile
+  output_directory = options.output_directory
+            
+  if not os.path.exists(logfile): 
+      raise RuntimeError('Log file does not exist: ' + logfile)
+  if not os.path.exists(output_directory):
+      raise RuntimeError('Output directory does not exist: ' + output_directory)
   
   # sort and uniq the log
   cmd = 'grep INFO %s | cut -f 4- -d " " | sort | uniq -c' % logfile
@@ -43,19 +46,14 @@ def main():
   
   # collect up global hashes and click counts
   details = dict()
-  domains = AutoVivification()
   for line in pipe.stdout:
     (count, global_hash, url) = line.strip().split(' ', 2)
-    agency = domain(url)
     details[global_hash] = dict(
       u=url,
-      global_clicks=count,
-      agency=agency,
+      global_clicks=long(count),
+      agency=domain(url),
       global_hash=global_hash,
     )
-    if not agency in domains:
-      domains['agency'] = dict(agency=agency, global_clicks=0)
-    domains['agency']['global_clicks'] = domains['agency']['global_clicks'] + int(count);
   
   # grab hashes in groups of GROUPSIZE size
   for i in xrange(1+len(details)/GROUPSIZE):
@@ -66,25 +64,17 @@ def main():
       details[info['hash']]['title']=info['title']
   
   # output files
-  json_file = output_directory + "/%04d-%02d-%02d.json" % \
-    (yesterday.year, yesterday.month, yesterday.day)
-  csv_file = output_directory + "/%04d-%02d-%02d.csv" % \
-    (yesterday.year, yesterday.month, yesterday.day)
-  domain_json_file = output_directory + "/domain-%04d-%02d-%02d.json" % \
-    (yesterday.year, yesterday.month, yesterday.day)
-  domain_csv_file = output_directory + "/domain-%04d-%02d-%02d.csv" % \
-    (yesterday.year, yesterday.month, yesterday.day)
+  json_file = output_directory + "/" + iso_date + ".json"
+  csv_file = output_directory + "/" + iso_date + ".csv"
+
   
   # sort by global clicks descending
   records = details.values()
   records.sort(key=lambda x: x["global_clicks"], reverse=True)  
 
-  domain_records = details.values()
-  domain_records.sort(key=lambda x: x["global_clicks"], reverse=True)  
 
   # write json
   json.dump(records, file(json_file, 'w'))
-  json.dump(domain_records, file(domain_json_file, 'w'))
   
   # write csv
   csv_writer = csv.writer(file(csv_file, 'w'))
@@ -99,13 +89,35 @@ def main():
       record['global_hash'],
     ])
 
-  csv_writer = csv.writer(file(domain_csv_file, 'w'))
+  if options.use_agency_domain:
+    write_agency(records, output_directory, iso_date)
+
+
+def write_agency(records, output_directory, iso_date):
+  domains = dict()
+  for record in records:
+    domain = domains.setdefault(record['agency'],
+                                dict(agency=record['agency'], global_clicks=0))
+    domain['global_clicks'] = domain['global_clicks'] + record['global_clicks']
+
+  domain_records = domains.values()
+  domain_records.sort(key=lambda x: x["global_clicks"], reverse=True)  
+
+  json_file = output_directory + "/domain-" + iso_date + ".json"
+  csv_file = output_directory + "/domain-" + iso_date + ".csv"
+
+  # write json
+  json.dump(domain_records, file(json_file, 'w'))
+
+  # write csv
+  csv_writer = csv.writer(file(csv_file, 'w'))
   csv_writer.writerow(["Agency Domain", "Clicks"])
   for record in domain_records:
-    if not 'title' in record: continue
+    if not 'agency' in record: continue
     csv_writer.writerow([
       record['agency'],
       record['global_clicks'],
     ])
+
 if __name__ == '__main__':
   main()
